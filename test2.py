4697948c81446da337a57ff1aa8805b2391cd6f3
Dimitris Maroulidis <dmaroulidis@dimitrismaroulidis.com>
2021-04-26 19:32:56 +0300

    Add second test example

---
+++ test2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import pandas as pd
+from algorithms.utastar import *
+
+
+multicrit_tbl = pd.read_csv("tests/cars.csv", index_col=0)
+multicrit_tbl.sort_values(by=["Διάταξη"], axis=0, inplace=True)
+crit_monot = {
+    "Τιμή": False,
+    "Κόστος Κυκλοφορίας ανά km": False,
+    "Ετήσιο κόστος ιδιοκτησίας": False,
+    "Ασφάλεια": True,
+    "Άνεση": True,
+}
+a_split = {
+    "Τιμή": 5,
+    "Κόστος Κυκλοφορίας ανά km": 3,
+    "Ετήσιο κόστος ιδιοκτησίας": 3,
+    "Ασφάλεια": 5,
+    "Άνεση": 5,
+}
