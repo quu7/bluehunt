d161721a847b83e1095369aaf3de98429465132b
Dimitris Maroulidis <dmaroulidis@dimitrismaroulidis.com>
2021-09-01 19:36:40 +0300

    Disable admin interface and debugging.

--- bluehunt/settings.py
+++ bluehunt/settings.py
@@ -23,16 +23,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-j%6tpf*d0%$fcy&n*+x8m9=68)47s4njs72-0c+ii4wcly15te"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "::1"]
 
 
 # Application definition
 
 INSTALLED_APPS = [
     "minora.apps.MinoraConfig",
-    "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
